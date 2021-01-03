b0861a5be6851757bfad45bb985c0c893d4a418a
Riveni <riven.eng@gmail.com>
2021-01-03 19:20:30 +0200

    Part 2

--- Hangman.py
+++ Hangman.py
@@ -1,5 +1,5 @@
-# Part 1
-print("""
+# Part 2.1
+HANGMAN_ASCII_ART = """
     _    _
    | |  | |
    | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
@@ -8,4 +8,10 @@ print("""
    |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                         __/ |
                        |___/
-""")
+"""
+MAX_TRIES = 6
+print(HANGMAN_ASCII_ART, MAX_TRIES, sep="\n")
+
+# Part 2.2
+user_guess = input("Guess a letter:")
+print(user_guess)
