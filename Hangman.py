f28a95c0b79ad0467e01e6a289fa4350318337b7
Riveni <riven.eng@gmail.com>
2021-01-04 23:07:13 +0200

    Part 4

--- Hangman.py
+++ Hangman.py
@@ -12,9 +12,15 @@ HANGMAN_ASCII_ART = """
 MAX_TRIES = 6
 print(HANGMAN_ASCII_ART, MAX_TRIES, sep="\n")
 
-# Part 2.2
-user_guess = input("Guess a letter:")
-print(user_guess.lower())
+# Part 2.2 + 4
+user_guess = input("Guess a letter:").lower()
+# check input validity:
+if len(user_guess) != 1:  # more then 1 letter
+    print("E1")
+elif ord(user_guess) < 97 or ord(user_guess) > 122:  # special char
+    print("E2")
+else:
+    print(user_guess)
 
 # Part 3
 secret_word = input("Please enter a word: ")
