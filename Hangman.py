00cf96fe910edc42ac3faeb1bf729353eb73f774
Riveni <riven.eng@gmail.com>
2021-01-05 20:56:01 +0200

    Part 5 - Using functions

--- Hangman.py
+++ Hangman.py
@@ -10,22 +10,89 @@ HANGMAN_ASCII_ART = """
                        |___/
 """
 MAX_TRIES = 6
-print(HANGMAN_ASCII_ART, MAX_TRIES, sep="\n")
 
-# Part 2.2 + 4
-user_guess = input("Guess a letter:").lower()
-# check input validity:
-if len(user_guess) != 1:  # more then 1 letter
-    if user_guess.isalpha():
-        print("E1")
+
+def game_state():
+    state1 = """
+        x-------x
+    """
+    state2 = """
+        x-------x
+        |
+        |
+        |
+        |
+        |
+    """
+    state3 = """
+        x-------x
+        |       |
+        |       0
+        |
+        |
+        |
+    """
+    state4 = """
+        x-------x
+        |       |
+        |       0
+        |       |
+        |
+        |
+    """
+    state5 = """
+        x-------x
+        |       |
+        |       0
+        |      /|\\
+        |
+        |
+    """
+    state6 = """
+        x-------x
+        |       |
+        |       0
+        |      /|\\
+        |      /
+        |
+    """
+    state7 = """
+        x-------x
+        |       |
+        |       0
+        |      /|\\
+        |      / \\
+        |
+    """
+    # print(state1, state2, state3, state4, state5, state6, state7, sep="\n")
+
+# Part 2.2 + 4 + 5
+def is_valid_input(letter_guessed):
+    """
+    function check the validity of the user input
+    if use input is longer than 1 char or is not a-z letter, return false
+    else return true
+    """
+    if (len(letter_guessed) != 1) or (not letter_guessed.isalpha()):
+        return False
     else:
-        print("E3")
-elif not user_guess.isalpha():  # special char
-    print("E2")
-else:
-    print(user_guess)
+        return True
+
 
 # Part 3
+"""
 secret_word = input("Please enter a word: ")
 print("_ " * len(secret_word))
+"""
 
+
+# Part 5
+def main():
+    print(HANGMAN_ASCII_ART, MAX_TRIES, sep="\n")
+    letter_guessed = input("Guess a letter:").lower()
+    print(is_valid_input(letter_guessed))
+    return
+
+
+if __name__ == "__main__":
+    main()
