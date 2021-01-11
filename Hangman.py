2e33580fe1a3564b1c980a197d747d2c1c82155e
Riveni <riven.eng@gmail.com>
2021-01-11 19:35:20 +0200

    Part 7: Check user status on the game

--- Hangman.py
+++ Hangman.py
@@ -100,6 +100,38 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed):
         return False
 
 
+# part 7
+def show_hidden_word(secret_word, old_letters_guessed):
+    """
+    show user his game status, letters he guessed correctly and number
+    of remaining letters to guess.
+    :param secret_word: string
+    :param old_letters_guessed: list
+    :return: string of partly the secret word
+    """
+    secret = list(secret_word)
+    i = 0
+    for letter in secret:
+        if not(letter in old_letters_guessed):
+            secret[i] = "_"
+        i += 1
+    return " ".join(secret)
+
+
+def check_win(secret_word, old_letters_guessed):
+    """
+    checks if user won the game by checking if all
+    letters in the secret word are guessed
+    :param secret_word: string
+    :param old_letters_guessed: list
+    :return: if won return True else False
+    """
+    for letter in secret_word:
+        if not(letter in old_letters_guessed):
+            return False
+    return True
+
+
 # Part 3
 """
 secret_word = input("Please enter a word: ")
@@ -110,10 +142,16 @@ print("_ " * len(secret_word))
 # Part 5
 def main():
     # print(HANGMAN_ASCII_ART, MAX_TRIES, sep="\n")
-    old_letters_guessed = ['a', 'p', 'c', 'f']
-    letter_guessed = input("Guess a letter:").lower()
+    # old_letters_guessed = ['s', 'p', 'j', 'i', 'm', 'k']
+    # letter_guessed = input("Guess a letter:").lower()
     # print(is_valid_input(letter_guessed, old_letters_guessed))
-    print(try_update_letter_guessed(letter_guessed, old_letters_guessed))
+    # print(try_update_letter_guessed(letter_guessed, old_letters_guessed))
+    # secret_word = "mammals"
+    # print(show_hidden_word(secret_word, old_letters_guessed))
+
+    secret_word = "yes"
+    old_letters_guessed = ['d', 'g', 'e', 'i', 's', 'k', 'y']
+    print(check_win(secret_word, old_letters_guessed))
     return
 
 
